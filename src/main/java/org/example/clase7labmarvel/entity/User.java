131d310c97347198044402ae5f23deed991993b5
GabrielTalledo <gabriel_alejandro.tm@hotmail.com>
2024-05-12 17:49:42 -0500

    feat: Funcional al 100%

--- src/main/java/org/example/clase7labmarvel/entity/User.java
+++ src/main/java/org/example/clase7labmarvel/entity/User.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @Entity
 @Table(name="users")
-public class User {
+public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id",nullable = false)
--- src/main/java/org/example/clase7labmarvel/repository/CharacterRepository.java
+++ src/main/java/org/example/clase7labmarvel/repository/CharacterRepository.java
@@ -1,6 +1,14 @@
 package org.example.clase7labmarvel.repository;
 
+import org.example.clase7labmarvel.entity.Character;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CharacterRepository extends JpaRepository<Character, Integer> {
+
+    List<Character> findByNameIgnoreCaseContaining(String search);
+
 }
