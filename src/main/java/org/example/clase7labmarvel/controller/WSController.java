39c08a7dd83277b4edbe8dd9a86d465ed430c626
GabrielTalledo <gabriel_alejandro.tm@hotmail.com>
2024-05-24 20:24:41 -0500

    feat: comienzo de conversion a rest2

--- src/main/java/org/example/clase7labmarvel/controller/WSController.java
+++ src/main/java/org/example/clase7labmarvel/controller/WSController.java
@@ -4,12 +4,10 @@ import org.example.clase7labmarvel.entity.Character;
 import org.example.clase7labmarvel.repository.CharacterRepository;
 import org.example.clase7labmarvel.repository.RoleRepository;
 import org.example.clase7labmarvel.repository.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
@@ -55,7 +53,6 @@ public class WSController {
             return ResponseEntity.badRequest().body(null);
         }
     }
-
 
 
 }
